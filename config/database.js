3237dc5194de8220657889bea162f72772fd83c7
roniceyemeli <roniceyemeli@gmail.com>
2023-09-03 01:05:54 +0100

    refactoring on the way

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
-const Sequelize = require('sequelize')
-const env = process.env.NODE_ENV || 'development'
-const config = require('./config')[env]
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("./config")[env];
 module.exports = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
-)
+);
