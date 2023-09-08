e8eec0a6980ecacc32fbbf5671094fe5ba1c3ea6
roniceyemeli <roniceyemeli@gmail.com>
2023-09-08 21:08:25 +0100

    new config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
-const Sequelize = require('sequelize')
-const env = process.env.NODE_ENV || 'development'
-const config = require('./config')[env]
-module.exports = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-)
+require("dotenv").config();
+const Sequelize = require("sequelize");
+const mysql2 = require("mysql2");
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DIALECT, // You can use 'mysql', 'postgres', 'sqlite', or 'mssql'
+    dialectModule: mysql2,
+    port: process.env.DB_PORT,
+    logging: false, // Disable SQL query logging (optional)
+  }
+);
+module.exports = sequelize;
