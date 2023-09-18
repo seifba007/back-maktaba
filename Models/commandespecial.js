dffc5afd304c0b2566ad1349b7484d37a295e638
softfiredev <braikbraik06@gmail.com>
2023-09-18 21:20:53 +0100

    version 18/09/2023

--- Models/commandespecial.js
+++ Models/commandespecial.js
@@ -6,5 +6,8 @@ module.exports = (db, DataTypes) => {
       Adresse :{type: DataTypes.INTEGER},
       Description : {type : DataTypes.STRING},
       Fichier : {type : DataTypes.STRING},
+      email: {type : DataTypes.STRING},
+      telephone: {type : DataTypes.INTEGER},
+      Nom:{type : DataTypes.STRING},
     });
 }
